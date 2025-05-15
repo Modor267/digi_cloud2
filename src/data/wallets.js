84771b8e613a870a39ec33ba670ad6ba0ac108ac
Modor <modordiscordeo@gmail.com>
2025-05-15 22:08:02 +0100

    Update wallets.js

--- src/data/wallets.js
+++ src/data/wallets.js
@@ -1,263 +1,263 @@
 export const wallets = [
 	{
 		name: 'binance smart chain',
-		logo: '/images/walletx-imgs/binance.jpg',
+		logo: `${import.meta.env.BASE_URL }images/walletx-imgs/binance.jpg`,
 	},
 	{
 		name: 'walletConnect',
-		logo: '/images/walletx-imgs/walletconnect.png',
+		logo: `${import.meta.env.BASE_URL}images/walletx-imgs/walletconnect.png`,
 	},
 	{
 		name: 'solana',
-		logo: '/images/walletx-imgs/solana.png',
+		logo: `${import.meta.env.BASE_URL}images/walletx-imgs/solana.png`,
 	},
 	{
 		name: 'metamask',
-		logo: '/images/walletx-imgs/metamask.jpg',
+		logo: `${import.meta.env.BASE_URL}images/walletx-imgs/metamask.jpg`,
 	},
 	{
 		name: 'coinbase',
-		logo: '/images/walletx-imgs/coinbase.jpg',
+		logo: `${import.meta.env.BASE_URL}images/walletx-imgs/coinbase.jpg`,
 	},
 	{
 		name: 'keplr',
-		logo: '/images/walletx-imgs/keplr.jpg',
+		logo: `${import.meta.env.BASE_URL}images/walletx-imgs/keplr.jpg`,
 	},
 	{
 		name: 'trust wallet',
-		logo: '/images/walletx-imgs/trust-wallet.jpg',
+		logo: `${import.meta.env.BASE_URL}images/walletx-imgs/trust-wallet.jpg`,
 	},
 	{
 		name: 'maiar',
-		logo: '/images/walletx-imgs/maiar.png',
+		logo: `${import.meta.env.BASE_URL}images/walletx-imgs/maiar.png`,
 	},
 	{
 		name: 'nomic',
-		logo: '/images/walletx-imgs/nomic.png',
+		logo: `${import.meta.env.BASE_URL}images/walletx-imgs/nomic.png`,
 	},
 	{
 		name: 'zoid pay',
-		logo: '/images/walletx-imgs/zoidpay.png',
+		logo: `${import.meta.env.BASE_URL}images/walletx-imgs/zoidpay.png`,
 	},
 	// {
 	// 	name: 'coincheck',
-	// 	logo: '/images/walletx-imgs/coincheck.svg',
+	// 	logo: `${import.meta.env.BASE_URL}images/walletx-imgs/coincheck.svg`,
 	// },
 	{
 		name: 'rainbow',
-		logo: '/images/walletx-imgs/rainbow.jpg',
+		logo: `${import.meta.env.BASE_URL}images/walletx-imgs/rainbow.jpg`,
 	},
 	{
 		name: 'argent',
-		logo: '/images/walletx-imgs/argent.jpg',
+		logo: `${import.meta.env.BASE_URL}images/walletx-imgs/argent.jpg`,
 	},
 	{
 		name: 'gnosis safe multisig',
-		logo: '/images/walletx-imgs/gnosis.jpg',
+		logo: `${import.meta.env.BASE_URL}images/walletx-imgs/gnosis.jpg`,
 	},
 	{
 		name: 'crypto.com | defi wallet',
-		logo: '/images/walletx-imgs/defi.jpg',
+		logo: `${import.meta.env.BASE_URL}images/walletx-imgs/defi.jpg`,
 	},
 	{
 		name: 'pillar',
-		logo: '/images/walletx-imgs/pillar.jpg',
+		logo: `${import.meta.env.BASE_URL}images/walletx-imgs/pillar.jpg`,
 	},
 	{
 		name: 'imtoken',
-		logo: '/images/walletx-imgs/imtoken.jpg',
+		logo: `${import.meta.env.BASE_URL}images/walletx-imgs/imtoken.jpg`,
 	},
 	{
 		name: 'ONTO',
-		logo: '/images/walletx-imgs/onto.jpg',
+		logo: `${import.meta.env.BASE_URL}images/walletx-imgs/onto.jpg`,
 	},
 	{
 		name: 'tokenPocket',
-		logo: '/images/walletx-imgs/tokenpocket.jpg',
+		logo: `${import.meta.env.BASE_URL}images/walletx-imgs/tokenpocket.jpg`,
 	},
 	{
 		name: 'MathWallet',
-		logo: '/images/walletx-imgs/mathwallet.jpg',
+		logo: `${import.meta.env.BASE_URL}images/walletx-imgs/mathwallet.jpg`,
 	},
 	{
 		name: 'bitpay',
-		logo: '/images/walletx-imgs/bitpay.jpg',
+		logo: `${import.meta.env.BASE_URL}images/walletx-imgs/bitpay.jpg`,
 	},
 	{
 		name: 'Ledger Live',
-		logo: '/images/walletx-imgs/ledgerlive.jpg',
+		logo: `${import.meta.env.BASE_URL}images/walletx-imgs/ledgerlive.jpg`,
 	},
 	{
 		name: 'walleth',
-		logo: '/images/walletx-imgs/walleth.jpg',
+		logo: `${import.meta.env.BASE_URL}images/walletx-imgs/walleth.jpg`,
 	},
 	{
 		name: 'authereum',
-		logo: '/images/walletx-imgs/authereum.jpg',
+		logo: `${import.meta.env.BASE_URL}images/walletx-imgs/authereum.jpg`,
 	},
 	{
 		name: 'dharma',
-		logo: '/images/walletx-imgs/dharma.jpg',
+		logo: `${import.meta.env.BASE_URL}images/walletx-imgs/dharma.jpg`,
 	},
 	{
 		name: '1inch wallet',
-		logo: '/images/walletx-imgs/1inchwallet.jpg',
+		logo: `${import.meta.env.BASE_URL}images/walletx-imgs/1inchwallet.jpg`,
 	},
 	{
 		name: 'huobi wallet',
-		logo: '/images/walletx-imgs/huobiwallet.jpg',
+		logo: `${import.meta.env.BASE_URL}images/walletx-imgs/huobiwallet.jpg`,
 	},
 	{
 		name: 'eidoo',
-		logo: '/images/walletx-imgs/eidoo.jpg',
+		logo: `${import.meta.env.BASE_URL}images/walletx-imgs/eidoo.jpg`,
 	},
 	{
 		name: 'etherscan',
-		logo: '/images/walletx-imgs/etherscan.jpg',
+		logo: `${import.meta.env.BASE_URL}images/walletx-imgs/etherscan.jpg`,
 	},
 	{
 		name: 'uniswap',
-		logo: '/images/walletx-imgs/uniswap.jpg',
+		logo: `${import.meta.env.BASE_URL}images/walletx-imgs/uniswap.jpg`,
 	},
 	{
 		name: 'binance dex',
-		logo: '/images/walletx-imgs/binancedex.jpg',
+		logo: `${import.meta.env.BASE_URL}images/walletx-imgs/binancedex.jpg`,
 	},
 	{
 		name: 'openSea',
-		logo: '/images/walletx-imgs/opensea.jpg',
+		logo: `${import.meta.env.BASE_URL}images/walletx-imgs/opensea.jpg`,
 	},
 	{
 		name: 'compound',
-		logo: '/images/walletx-imgs/compound.jpg',
+		logo: `${import.meta.env.BASE_URL}images/walletx-imgs/compound.jpg`,
 	},
 	{
 		name: 'zapper',
-		logo: '/images/walletx-imgs/zapper.jpg',
+		logo: `${import.meta.env.BASE_URL}images/walletx-imgs/zapper.jpg`,
 	},
 	{
 		name: 'aave',
-		logo: '/images/walletx-imgs/aave.jpg',
+		logo: `${import.meta.env.BASE_URL}images/walletx-imgs/aave.jpg`,
 	},
 	{
 		name: 'zerion',
-		logo: '/images/walletx-imgs/zerion.jpg',
+		logo: `${import.meta.env.BASE_URL}images/walletx-imgs/zerion.jpg`,
 	},
 	{
 		name: 'localcryptos',
-		logo: '/images/walletx-imgs/localcryptos.jpg',
+		logo: `${import.meta.env.BASE_URL}images/walletx-imgs/localcryptos.jpg`,
 	},
 	{
 		name: 'debank',
-		logo: '/images/walletx-imgs/debank.jpg',
+		logo: `${import.meta.env.BASE_URL}images/walletx-imgs/debank.jpg`,
 	},
 	{
 		name: 'unstappable domains',
-		logo: '/images/walletx-imgs/unstappable.jpg',
+		logo: `${import.meta.env.BASE_URL}images/walletx-imgs/unstappable.jpg`,
 	},
 	{
 		name: 'rarible',
-		logo: '/images/walletx-imgs/rarible.jpg',
+		logo: `${import.meta.env.BASE_URL}images/walletx-imgs/rarible.jpg`,
 	},
 	{
 		name: '1inch exchange',
-		logo: '/images/walletx-imgs/1inchexchange.jpg',
+		logo: `${import.meta.env.BASE_URL}images/walletx-imgs/1inchexchange.jpg`,
 	},
 	{
 		name: 'yearn',
-		logo: '/images/walletx-imgs/yearn.jpg',
+		logo: `${import.meta.env.BASE_URL}images/walletx-imgs/yearn.jpg`,
 	},
 	{
 		name: 'nash',
-		logo: '/images/walletx-imgs/nash.jpg',
+		logo: `${import.meta.env.BASE_URL}images/walletx-imgs/nash.jpg`,
 	},
 	{
 		name: 'curve',
-		logo: '/images/walletx-imgs/curve.jpg',
+		logo: `${import.meta.env.BASE_URL}images/walletx-imgs/curve.jpg`,
 	},
 	{
 		name: 'adex network',
-		logo: '/images/walletx-imgs/adexnetwork.jpg',
+		logo: `${import.meta.env.BASE_URL}images/walletx-imgs/adexnetwork.jpg`,
 	},
 	{
 		name: 'dYdX',
-		logo: '/images/walletx-imgs/dydx.jpg',
+		logo: `${import.meta.env.BASE_URL}images/walletx-imgs/dydx.jpg`,
 	},
 	{
 		name: 'myCrypto',
-		logo: '/images/walletx-imgs/myCrypto.jpg',
+		logo: `${import.meta.env.BASE_URL}images/walletx-imgs/myCrypto.jpg`,
 	},
 	{
 		name: 'mykey',
-		logo: '/images/walletx-imgs/mykey.jpg',
+		logo: `${import.meta.env.BASE_URL}images/walletx-imgs/mykey.jpg`,
 	},
 	{
 		name: 'looping',
-		logo: '/images/walletx-imgs/looping.jpg',
+		logo: `${import.meta.env.BASE_URL}images/walletx-imgs/looping.jpg`,
 	},
 	{
 		name: 'trustVault',
-		logo: '/images/walletx-imgs/trustVault.jpg',
+		logo: `${import.meta.env.BASE_URL}images/walletx-imgs/trustVault.jpg`,
 	},
 	{
 		name: 'atomic',
-		logo: '/images/walletx-imgs/atomic.jpg',
+		logo: `${import.meta.env.BASE_URL}images/walletx-imgs/atomic.jpg`,
 	},
 	{
 		name: 'coin98',
-		logo: '/images/walletx-imgs/coin98.jpg',
+		logo: `${import.meta.env.BASE_URL}images/walletx-imgs/coin98.jpg`,
 	},
 	{
 		name: 'coolwallet s',
-		logo: '/images/walletx-imgs/coolwallet.jpg',
+		logo: `${import.meta.env.BASE_URL}images/walletx-imgs/coolwallet.jpg`,
 	},
 	{
 		name: 'alice',
-		logo: '/images/walletx-imgs/alice.jpg',
+		logo: `${import.meta.env.BASE_URL}images/walletx-imgs/alice.jpg`,
 	},
 	{
 		name: 'alphaWallet',
-		logo: '/images/walletx-imgs/alphawallet.jpg',
+		logo: `${import.meta.env.BASE_URL}images/walletx-imgs/alphawallet.jpg`,
 	},
 	{
 		name: `D'CENT wallet`,
-		logo: '/images/walletx-imgs/dcent.jpg',
+		logo: `${import.meta.env.BASE_URL}images/walletx-imgs/dcent.jpg`,
 	},
 	{
 		name: 'zelcore',
-		logo: '/images/walletx-imgs/zelcore.jpg',
+		logo: `${import.meta.env.BASE_URL}images/walletx-imgs/zelcore.jpg`,
 	},
 	{
 		name: 'coinomi',
-		logo: '/images/walletx-imgs/coinomi.jpg',
+		logo: `${import.meta.env.BASE_URL}images/walletx-imgs/coinomi.jpg`,
 	},
 	{
 		name: 'gridPlus',
-		logo: '/images/walletx-imgs/gridPlus.jpg',
+		logo: `${import.meta.env.BASE_URL}images/walletx-imgs/gridPlus.jpg`,
 	},
 	{
 		name: 'CYBAVO wallet',
-		logo: '/images/walletx-imgs/cybavowallet.jpg',
+		logo: `${import.meta.env.BASE_URL}images/walletx-imgs/cybavowallet.jpg`,
 	},
 	{
 		name: 'tokenary',
-		logo: '/images/walletx-imgs/tokenary.jpg',
+		logo: `${import.meta.env.BASE_URL}images/walletx-imgs/tokenary.jpg`,
 	},
 	{
 		name: 'torus',
-		logo: '/images/walletx-imgs/torus.jpg',
+		logo: `${import.meta.env.BASE_URL}images/walletx-imgs/torus.jpg`,
 	},
 	{
 		name: 'spatium',
-		logo: '/images/walletx-imgs/spatium.jpg',
+		logo: `${import.meta.env.BASE_URL}images/walletx-imgs/spatium.jpg`,
 	},
 	{
 		name: 'safePal',
-		logo: '/images/walletx-imgs/safePal.jpg',
+		logo: `${import.meta.env.BASE_URL}images/walletx-imgs/safePal.jpg`,
 	},
 	{
 		name: 'infinito',
-		logo: '/images/walletx-imgs/infinito.jpg',
+		logo: `${import.meta.env.BASE_URL}images/walletx-imgs/infinito.jpg`,
 	},
 
 ];
